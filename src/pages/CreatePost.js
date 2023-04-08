fbb11a9f33de2edbf10d4938d30be57de3f8913e
paul-nguyen96 <paul121596@icloud.com>
2023-04-08 15:33:55 -0500

    connect the db and database entry

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,27 +1,82 @@
-import React from 'react';
-import './CreatePost.css'
+import React, { useState } from "react";
+import "./CreatePost.css";
+import { supabase } from "../client";
 
 const CreatePost = () => {
+  const [input, setInput] = useState({
+    title: "",
+    author: "",
+    description: "",
+  });
 
-    return (
-        <div>
-            <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" /><br />
-                <br/>
+  const handleChange = (e) => {
+    const name = e.target.name;
+    setInput((prev) => {
+      return { ...prev, [name]: e.target.value };
+    });
+    console.log(input);
+  };
 
-                <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" /><br />
-                <br/>
+  const createPost = async (event) => {
+    event.preventDefault();
 
-                <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description">
-                </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
-}
+    await supabase
+      .from("Posts")
+      .insert({
+        title: input.title,
+        author: input.author,
+        description: input.description,
+      })
+      .select();
 
-export default CreatePost
+    window.location = "/";
+
+    setInput({
+      title: "",
+      author: "",
+      description: "",
+    });
+  };
+
+  return (
+    <div>
+      <form>
+        <label for="title">Title</label> <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={input.title}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label for="author">Author</label>
+        <br />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={input.author}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label for="description">Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          value={input.description}
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" onClick={createPost} />
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
