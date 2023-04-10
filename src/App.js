7b70ba532f2d922350a4c7fcf4cc27a63c33dc77
paul-nguyen96 <paul121596@icloud.com>
2023-04-09 20:42:11 -0500

    mini crud bet application

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import CreatePost from './pages/CreatePost'
 import EditPost from './pages/EditPost'
 import { Link } from 'react-router-dom'
 
-
 const App = () => {
   
   const descr = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -1,32 +1,95 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import './EditPost.css'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import "./EditPost.css";
+import { supabase } from "../client";
+import { useEffect } from "react";
 
-const EditPost = ({data}) => {
+const EditPost = ({ data }) => {
+  const { id } = useParams();
+  const [post, setPost] = useState({});
 
-    const {id} = useParams();
-    const post = data.filter(item => item.id === id)[0];
+  const updatePost = async (e) => {
+    e.preventDefault();
 
-    return (
-        <div>
-            <form>
-                <label for="title">Title</label> <br />
-                <input type="text" id="title" name="title" value={post.title} /><br />
-                <br/>
+    await supabase
+      .from("Posts")
+      .update({
+        title: post.title,
+        author: post.author,
+        description: post.description,
+      })
+      .eq("id", id);
 
-                <label for="author">Author</label><br />
-                <input type="text" id="author" name="author" value={post.author} /><br />
-                <br/>
+    window.location = "/";
+  };
 
-                <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} >
-                </textarea>
-                <br/>
-                <input type="submit" value="Submit" />
-                <button className="deleteButton">Delete</button>
-            </form>
-        </div>
-    )
-}
+  const deletePost = async (e) => {
+    e.preventDefault();
+    await supabase.from("Posts").delete().eq("id", id);
+    window.location = "/";
+  };
 
-export default EditPost
+  const handleChange = (e) => {
+    const name = e.target.name;
+    setPost((prev) => {
+      return { ...prev, [name]: e.target.value };
+    });
+  };
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const { data, error } = await supabase
+        .from("Posts")
+        .select()
+        .eq("id", id);
+      // console.log(data);
+      setPost(data[0]);
+    };
+    fetchPost();
+  }, []);
+
+  return (
+    <div>
+      <form>
+        <label for="title">Title</label> <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={post.title}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label for="author">Author</label>
+        <br />
+        <input
+          type="text"
+          id="author"
+          name="author"
+          value={post.author}
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label for="description">Description</label>
+        <br />
+        <textarea
+          rows="5"
+          cols="50"
+          id="description"
+          name="description"
+          value={post.description}
+          onChange={handleChange}
+        ></textarea>
+        <br />
+        <input type="submit" value="Submit" onClick={updatePost} />
+        <button className="deleteButton" onClick={deletePost}>
+          Delete
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditPost;
